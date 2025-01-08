955ccb7282f2e979a371b5240442c24194b16062
taguhome <147036087+taguhome@users.noreply.github.com>
2025-01-08 17:22:44 +0900

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -196,7 +196,7 @@ VSCode + \LaTeX の環境構築が完了しました！
 #| ForEach-Object { [Text.Encoding]::UTF8.GetBytes($_) }
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 
-  runas /machine:$(${env:PROCESSOR_ARCHITECTURE}.ToLower()) /trustlevel:0x20000 "$vscodeExePath `"$examplelatexDir`" `"$examplelatexDir/$exampleName`""
+  runas /machine:$(${env:PROCESSOR_ARCHITECTURE}.ToLower()) /trustlevel:0x40000 "$vscodeExePath `"$examplelatexDir`" `"$examplelatexDir/$exampleName`""
 
 
   
