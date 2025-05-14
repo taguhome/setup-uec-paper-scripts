b3be94b6cb63785e7593f09a803f153a79ddcd55
taguhome <147036087+taguhome@users.noreply.github.com>
2025-05-14 15:34:39 +0900

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -67,46 +67,46 @@ function Copy-AdditionalFiles() {
    
 $latexmkrcContent = @'
     # 通常の LaTeX ドキュメントのビルドコマンド
-    ${0}latex = 'uplatex %O -kanji=utf8 -no-guess-input-enc -synctex=1 -interaction=nonstopmode %S';
+    ${1}latex = 'uplatex %O -kanji=utf8 -no-guess-input-enc -synctex=1 -interaction=nonstopmode %S';
     #
-    ${0}uplatex = 'uplatex %O  -synctex=1 -interaction=nonstopmode %S';
+    ${1}uplatex = 'uplatex %O  -synctex=1 -interaction=nonstopmode %S';
     # pdfLaTeX のビルドコマンド
-    ${0}pdflatex = 'pdflatex %O -synctex=1 -interaction=nonstopmode %S';
+    ${1}pdflatex = 'pdflatex %O -synctex=1 -interaction=nonstopmode %S';
     # LuaLaTeX のビルドコマンド
-    ${0}lualatex = 'lualatex %O -synctex=1 -interaction=nonstopmode %S';
+    ${1}lualatex = 'lualatex %O -synctex=1 -interaction=nonstopmode %S';
     # XeLaTeX のビルドコマンド
-    ${0}xelatex = 'xelatex %O -no-pdf -synctex=1 -shell-escape -interaction=nonstopmode %S';
+    ${1}xelatex = 'xelatex %O -no-pdf -synctex=1 -shell-escape -interaction=nonstopmode %S';
     # Biber, BibTeX のビルドコマンド
-    ${0}biber = 'biber %O --bblencoding=utf8 -u -U --output_safechars %B';
-    #${0}bibtex = 'upbibtex %O %B';
-    ${0}platex= 'platex %O -synctex=1 -halt-on-error -interaction=nonstopmode -file-line-error %O %S';
-    #${0}bibtex = 'upbibtex %O %B';
-    ${0}bibtex = 'pbibtex %O %B';
+    ${1}biber = 'biber %O --bblencoding=utf8 -u -U --output_safechars %B';
+    #${1}bibtex = 'upbibtex %O %B';
+    ${1}platex= 'platex %O -synctex=1 -halt-on-error -interaction=nonstopmode -file-line-error %O %S';
+    #${1}bibtex = 'upbibtex %O %B';
+    ${1}bibtex = 'pbibtex %O %B';
     # makeindex のビルドコマンド
-    ${0}makeindex = 'upmendex %O -o %D %S';
+    ${1}makeindex = 'upmendex %O -o %D %S';
     # dvipdf のビルドコマンド
-    ${0}dvipdf = 'dvipdfmx %O -o %D %S';
+    ${1}dvipdf = 'dvipdfmx %O -o %D %S';
     # dvipd のビルドコマンド
-    ${0}dvips = 'dvips %O -z -f %S | convbkmk -u > %D';
-    ${0}ps2pdf = 'ps2pdf.exe %O %S %D';
+    ${1}dvips = 'dvips %O -z -f %S | convbkmk -u > %D';
+    ${1}ps2pdf = 'ps2pdf.exe %O %S %D';
 
     # PDF の作成方法を指定するオプション
-    ## ${0}pdf_mode = 0; PDF を作成しない。
-    ## ${0}pdf_mode = 1; $pdflatex を利用して PDF を作成。
-    ## ${0}pdf_mode = 2; $ps2pdf を利用して .ps ファイルから PDF を作成。
-    ## ${0}pdf_mode = 3; $dvipdf を利用して .dvi ファイルから PDF を作成。
-    ## ${0}pdf_mode = 4; $lualatex を利用して .dvi ファイルから PDF を作成。
-    ## ${0}pdf_mode = 5; xdvipdfmx を利用して .xdv ファイルから PDF を作成。
-    ${0}pdf_mode = 4;
+    ## ${1}pdf_mode = 0; PDF を作成しない。
+    ## ${1}pdf_mode = 1; $pdflatex を利用して PDF を作成。
+    ## ${1}pdf_mode = 2; $ps2pdf を利用して .ps ファイルから PDF を作成。
+    ## ${1}pdf_mode = 3; $dvipdf を利用して .dvi ファイルから PDF を作成。
+    ## ${1}pdf_mode = 4; $lualatex を利用して .dvi ファイルから PDF を作成。
+    ## ${1}pdf_mode = 5; xdvipdfmx を利用して .xdv ファイルから PDF を作成。
+    ${1}pdf_mode = 4;
 
     # PDF viewer の設定
-    ${0}pdf_previewer = "start %S";  # "start %S": .pdf に関連付けられた既存のソフトウェアで表示する。
+    ${1}pdf_previewer = "start %S";  # "start %S": .pdf に関連付けられた既存のソフトウェアで表示する。
 
     # Windows では SyncTeX(PDF をビューアーで開いたまま中身の更新が可能で更新がビューアーで反映される機能) が利用できる SumatraPDF 等が便利。
     # ぜひ SyncTeX 機能のあるビューアーをインストールしよう。
     # SumatraPDF: https://www.sumatrapdfreader.org/free-pdf-reader.html
-    # ${0}pdf_previewer = '{1} -reuse-instance';
-'@ -f '$', $sumatraPath 
+    # ${1}pdf_previewer = '{0} -reuse-instance';
+'@ -f $sumatraPath ,'$'
 
 $latexmkrcContent | Out-File  -FilePath "$env:USERPROFILE/.vscode/$latexmkrcName" 　-Encoding UTF8
 
